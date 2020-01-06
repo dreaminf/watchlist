f5a1ce015e1f354d04dcadf9db3f0e80d4cbad40
zhenghl <zhenghl1126@126.com>
2020-01-06 22:15:04 +0800

    添加自定义命令 ，删除 创建数据库

--- README.md
+++ README.md
@@ -24,3 +24,28 @@ SQLAlchemy——一个 Python 数据库工具 （ORM，即对象关系映射）�
 提供了一个统一的接口来写入和获取这些配置变 量：
  Flask.config 字典。配置变量的名称必须使用大写，
 写入配置的语句一般 会放到扩展类实例化语句之前。
+
+# 模型类编写
+模型类的编写有一些限制： 
+模型类要声明继承 db.Model 。
+ 每一个类属性（字段）要实例化 db.Column ，
+ 传入的参数为字段的类型，下 面的表格列出了常用的字段类。
+  在 db.Column() 中添加额外的选项（参数）可以对字段进行设置。比 如， primary_key 设置当前字段是否为主键。除此之外，常用的选项还有 nullable （布尔值，是否允许为空值）、 index （布尔值，是否设置索 引）、 unique （布尔值，是否允许重复值）、 default （设置默认值） 等。 常用的字段类型如下表所示： 字段类 说明 db.Integer 整型 db.String (size) 字符串，size 为最大长度，比如 db.String(20) db.Text 长文本 db.DateTime 时间日期，Python datetime 对象 db.Float 浮点数 db.Boolean 布尔值
+
+```python
+class User(db.Model):  # 表名会是user (自动生成。小写处理)
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    name = db.Column(db.String(20))  # 名字
+
+
+class Movie(db.Model):  # 表名会是movie
+    id = db.Column(db.Integer, primary_key=type)  #主键
+    title = db.Column(db.String(60))  # 电影标题
+    year = db.Column(db.String(4))    # 电影年份
+```
+```python
+flask shell
+from app import db
+db.create_all()
+db.drop_all()
+```
--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
 import sys
 from flask import Flask, render_template
-from flask import url_for
+import click
+# from flask import url_for
 from flask_sqlalchemy import SQLAlchemy  # 导入扩展类
 WIN = sys.platform.startswith('win')
 if WIN:
@@ -15,6 +16,29 @@ app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(
 )
 app.config['SQLALCHEMY_TRACE_MODIFICATIONS'] = False  # 关闭对模型修改的监控
 db = SQLAlchemy(app)  # 初始化扩展 ，传入程序实例
+
+
+class User(db.Model):  # 表名会是user (自动生成。小写处理)
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    name = db.Column(db.String(20))  # 名字
+
+
+class Movie(db.Model):  # 表名会是movie
+    id = db.Column(db.Integer, primary_key=type)  #主键
+    title = db.Column(db.String(60))  # 电影标题
+    year = db.Column(db.String(4))    # 电影年份
+
+
+# 自定义命令 initdb
+@app.cli.command() # 注册为命令
+@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
+def initdb(drop):
+    """Initialize the database."""
+    if drop:    # 判断是否出入了选项
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized databases.')  # 输出提示信息
+
 
 name = 'Zhenghl'
 # 列表
@@ -30,6 +54,7 @@ movies = [
     {'title': 'WALL-E', 'year': '1981'},
     {'title': 'The Pork of Music', 'year': '1984'},
 ]
+
 
 
 @app.route('/')
